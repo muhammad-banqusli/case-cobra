dc00ddf9a228d8008f64bd6e2e92f061768ccd7c
--replace-all <mohamad.banksli@ogr.dpu.edu.tr>
2024-07-06 23:34:19 +0300

    update resend

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -71,7 +71,7 @@ export async function POST(req: NextRequest) {
             });
 
             const { error } = await resend.emails.send({
-                from: "CaseCobra <casecobra@casecobra.resend.com>",
+                from: "CaseCobra <onboarding@resend.dev>",
                 to: [event.data.object.customer_details.email],
                 subject: "Thanks for your order!",
                 react: OrderReceivedEmail({
