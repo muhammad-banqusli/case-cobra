27f3e57660cad62bbdcdf4173b0dd596ab54e3ea
--replace-all <mohamad.banksli@ogr.dpu.edu.tr>
2024-07-06 12:17:10 +0300

    update thank you page

---
+++ src/app/dashborad/page.tsx
@@ -0,0 +1,3 @@
+const Page = () => {
+    return <div className="flex min-h-screen w-full bg-muted/40"></div>;
+};
--- src/components/ThankYou.tsx
+++ src/components/ThankYou.tsx
@@ -5,6 +5,7 @@ import { useQuery } from "@tanstack/react-query";
 import { Loader2 } from "lucide-react";
 import { useSearchParams } from "next/navigation";
 import { PhonePreview } from "@/components";
+import { formatPrice } from "@/lib/utils";
 
 export default function ThankYou() {
     const searchParams = useSearchParams();
@@ -83,6 +84,85 @@ export default function ThankYou() {
                         croppedImageUrl={configuration.croppedImageUrl!}
                         color={color!}
                     />
+                </div>
+                <div>
+                    <div className="grid grid-cols-2 gap-x-6 py-10 text-sm">
+                        <div>
+                            <p className="font-medium text-gray-900">
+                                Shipping address
+                            </p>
+                            <div className="mt-2 text-zinc-700">
+                                <address className="not-italic">
+                                    <span className="block">
+                                        {ShippingAddress?.name}
+                                    </span>
+                                    <span className="block">
+                                        {ShippingAddress?.street}
+                                    </span>
+                                    <span className="block">
+                                        {ShippingAddress?.postalCode}{" "}
+                                        {ShippingAddress?.city}
+                                    </span>
+                                </address>
+                            </div>
+                        </div>
+                        <div>
+                            <p className="font-medium text-gray-900">
+                                Billing address
+                            </p>
+                            <div className="mt-2 text-zinc-700">
+                                <address className="not-italic">
+                                    <span className="block">
+                                        {BillingAddress?.name}
+                                    </span>
+                                    <span className="block">
+                                        {BillingAddress?.street}
+                                    </span>
+                                    <span className="block">
+                                        {BillingAddress?.postalCode}{" "}
+                                        {BillingAddress?.city}
+                                    </span>
+                                </address>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid grid-cols-2 gap-x-6 border-t border-zinc-200 py-10 text-sm">
+                    <div>
+                        <p className="font-medium">Payment Status</p>
+                        <p className="mt-2 text-zinc-700">Paid</p>
+                    </div>
+
+                    <div>
+                        <p className="font-medium">Shipping Method</p>
+                        <p className="mt-2 text-zinc-700">DHL, takes up to 3 working days</p>
+                    </div>
+                </div>
+                <div className="space-y-6 border-t border-zinc-200 pt-10 text-sm">
+                    <div className="flex justify-between">
+                        <p className="font-medium text-zinc-900">
+                            Subtotal
+                        </p>
+                        <p className="text-zinc-700">
+                            {formatPrice(amount)}
+                        </p>
+                    </div>
+                    <div className="flex justify-between">
+                        <p className="font-medium text-zinc-900">
+                            Shipping
+                        </p>
+                        <p className="text-zinc-700">
+                            {formatPrice(0)}
+                        </p>
+                    </div>
+                    <div className="flex justify-between">
+                        <p className="font-medium text-zinc-900">
+                            Total
+                        </p>
+                        <p className="text-zinc-700">
+                            {formatPrice(amount)}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
